df40a78aa377c9d46fa917ab763a804875d8a824
Sohyun Park <100866731+doridori-samsam@users.noreply.github.com>
2022-06-22 03:00:38 +0900

    Revert "🎨 이메일 회원가입 화면 구현 및 reset.scss파일 추가"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-
-//import { Reset } from 'styled-reset'// globalStyles를 쓰지 않고 scss를 쓸것이기 때문에 reset.scss 파일을 따로 추가했습니다.
-import '../src/Components/scss/reset.scss';
-import './app.scss';
-import Login from './Pages/Login/Login';
-
+import { Reset } from 'styled-reset'
+import './App.scss';
+import Login from './Pages/logIn/Login';
 
 function App() {
   return (
