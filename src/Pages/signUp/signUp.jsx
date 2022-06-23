1a67c1b120c02779f11e7edf5792d682606f8f3e
Sam <wtawonlife@gmail.com>
2022-06-23 12:52:07 +0900

    🔧 이메일 회원가입에서 이메일/비밀번호 입력 시 다음버튼 활성화 기능 추가

--- src/Pages/signUp/signUp.jsx
+++ src/Pages/signUp/signUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './signUp.scss';
 
 function SignUp(){
@@ -11,39 +11,41 @@ function SignUp(){
 }
 
 function InputSection(){
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    function handleIdInput(e){
+        setEmail(e.target.value);
+    }
+    
+    function handlePasswordInput(e){
+        setPassword(e.target.value);
+    }
+    
+    const [isActive, setIsActive] = useState(true);
+   
+    function goodToJoin(){
+        return email.length>=7 && password.length>=7 ? setIsActive(false) : setIsActive(true);
+    }
+
     return(
         <article>
             <form method="POST">
-            <InputEmail/>
-            <InputPassword/>
-            <NextButton/>
+                <div className="email-signup">
+                 <label for="input-email">이메일</label>
+                 <input onKeyUp={goodToJoin} onChange={handleIdInput} type="email" name="user-email" id="input-email" placeholder="이메일 주소를 입력해 주세요."></input>
+                 </div>
+                 <div className="pwd-signup">
+                  <label for="input-pwd">비밀번호</label>
+                  <input onKeyUp={goodToJoin} onChange={handlePasswordInput} type="password" name="user-pwd" id="input-pwd" maxLength="16" placeholder="비밀번호를 설정해 주세요."></input>
+                </div>
+                <button type="submit" formtarget="#none" className={`next-btn ${isActive}`} disabled={isActive}>다음</button>
             </form>
         </article>
     )
 }
 
-function InputEmail(){
-    return(
-        <div className="email-signup">
-            <label for="input-email">이메일</label>
-            <input type="email" name="user-email" id="input-email" placeholder="이메일 주소를 입력해 주세요."></input>
-        </div>
-    )
-}
-
-function InputPassword(){
-    return(
-        <div className="pwd-signup">
-            <label for="input-pwd">비밀번호</label>
-            <input type="password" name="user-pwd" id="input-pwd" maxlength="16" placeholder="비밀번호를 설정해 주세요."></input>
-        </div>
-    )
-}
-
-function NextButton(){
-    return(
-        <button type="submit" formtarget="#none" className="next-btn">다음</button>
-    )
-}
 
 export default SignUp;
+
