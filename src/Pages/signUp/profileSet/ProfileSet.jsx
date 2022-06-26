817728ac878320dd8456b8509a3cb4b69abf50d3
임현지 <imhyeonji@imhyeonjiui-MacBook-Pro.local>
2022-06-26 22:40:34 +0900

    🔧 useContext 이용하여 회원가입 정보 POST 기능 구현

--- src/Pages/signUp/profileSet/ProfileSet.jsx
+++ src/Pages/signUp/profileSet/ProfileSet.jsx
@@ -1,25 +1,121 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./profileSet.scss";
 import PictureIcon from "../../../assets/images/upload-file.svg";
 import Button from "../button/Button";
+import UserContext from "../contexts/UserContext";
 
 function ProfileSet() {
+  const [username, setUserName] = useState("");
+  const [accountname, setAcountName] = useState("");
+  const [intro, setIntro] = useState("");
+  const { user, setUser } = useContext(UserContext);
+  // 유저 정보 입력 함수
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUser({
+      ...user,
+      username: username,
+      accountname: accountname,
+      intro: intro,
+    });
+    submitUserInfo();
+  }
+  // 유저 정보 서버로 POST
+  function submitUserInfo(e) {
+    const userInfo = { user: "" };
+    userInfo.user = user;
+    fetch("https://mandarin.api.weniv.co.kr/user", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    }).then((res) => {
+      console.log(res);
+    });
+  }
+
   return (
     <>
-      <form method="POST" enctype="multipart/form-data">
+      <form method="POST" enctype="multipart/form-data" onSubmit={handleSubmit}>
         <h1>프로필 설정</h1>
         <h2>나중에 언제든지 변경할 수 있습니다.</h2>
-        <EditMain />
+        <EditInfo
+          username={username}
+          setUserName={setUserName}
+          accountname={accountname}
+          setAcountName={setAcountName}
+          intro={intro}
+          setIntro={setIntro}
+        />
       </form>
     </>
   );
 }
 
-function EditMain() {
+function EditInfo({
+  username,
+  setUserName,
+  accountname,
+  setAcountName,
+  intro,
+  setIntro,
+}) {
+  const [isActive, setIsActive] = useState(true);
+
+  function handleUserNameInput(e) {
+    setUserName(e.target.value);
+  }
+
+  function handleAcountNameInput(e) {
+    setAcountName(e.target.value);
+  }
+
+  function handleIntroInput(e) {
+    setIntro(e.target.value);
+  }
+
+  function goodToJoin() {
+    return username.length >= 2 && accountname.length >= 3
+      ? setIsActive(false)
+      : setIsActive(true);
+  }
+
   return (
     <>
       <UploadPic />
-      <EditInfo />
+      <div className="-container-input-info">
+        <label for="input-name">사용자 이름</label>
+        <input
+          type="text"
+          id="input-name"
+          name="user-name"
+          maxlength="10"
+          placeholder="2~10자 이내여야 합니다."
+          onKeyUp={goodToJoin}
+          onChange={handleUserNameInput}
+        ></input>
+        <label for="input-id">계정 ID</label>
+        <input
+          type="text"
+          id="input-id"
+          name="user-id"
+          placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다. "
+          onKeyUp={goodToJoin}
+          onChange={handleAcountNameInput}
+        ></input>
+        <label for="input-intro">소개</label>
+        <input
+          type="text"
+          id="input-intro"
+          name="user-intro"
+          placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
+          onChange={handleIntroInput}
+        ></input>
+      </div>
+      <Button type="submit" isActive={isActive}>
+        감귤마켓 시작하기
+      </Button>
     </>
   );
 }
@@ -37,62 +133,6 @@ function UploadPic() {
         accept="image/*"
       ></input>
     </div>
-  );
-}
-
-function EditInfo() {
-  const [name, setName] = useState("");
-  const [id, setId] = useState("");
-  const [isActive, setIsActive] = useState(true);
-
-  function handleNameInput(e) {
-    setName(e.target.value);
-  }
-
-  function handleIdInput(e) {
-    setId(e.target.value);
-  }
-
-  function goodToJoin() {
-    return name.length >= 2 && id.length >= 3
-      ? setIsActive(false)
-      : setIsActive(true);
-  }
-
-  return (
-    <>
-      <div className="-container-input-info">
-        <label for="input-name">사용자 이름</label>
-        <input
-          type="text"
-          id="input-name"
-          name="user-name"
-          maxlength="10"
-          placeholder="2~10자 이내여야 합니다."
-          onKeyUp={goodToJoin}
-          onChange={handleNameInput}
-        ></input>
-        <label for="input-id">계정 ID</label>
-        <input
-          type="text"
-          id="input-id"
-          name="user-id"
-          placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다. "
-          onKeyUp={goodToJoin}
-          onChange={handleIdInput}
-        ></input>
-        <label for="input-intro">소개</label>
-        <input
-          type="text"
-          id="input-intro"
-          name="user-intro"
-          placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
-        ></input>
-      </div>
-      <Button type="submit" isActive={isActive}>
-        감귤마켓 시작하기
-      </Button>
-    </>
   );
 }
 
