5b2908b666709a060ce7dd215a4b63f5dfbfa441
waterbinnn <ewaterbin@naver.com>
2022-06-28 02:26:48 +0900

    🔧:로그인페이지 기능 추가 구현

--- src/Pages/logIn/EmailLogin.jsx
+++ src/Pages/logIn/EmailLogin.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import "./EmailLogin.scss";
@@ -10,13 +10,29 @@ function EmailLogin() {
   const [isActive, setIsActive] = useState(true);
 
   const history = useHistory();
+  const inputRef = useRef();
+
+  const error = document.querySelector(".errorMsg");
+
+  //페이지 로딩됐을 때 이메일 인풋 포커스
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   const onEmailHandler = (event) => {
     setEmail(event.currentTarget.value);
+    error.style.display = "none";
   };
 
   const onPasswordHandler = (event) => {
-    setPassword(event.target.value);
+    setPassword(event.currentTarget.value);
+    error.style.display = "none";
+  };
+
+  // 경구문구 후 인풋값 리셋
+  const onReset = () => {
+    setEmail("");
+    setPassword("");
   };
 
   //이메일 주소 유효성 검사
@@ -36,7 +52,6 @@ function EmailLogin() {
 
     const email = document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
-    const error = document.querySelector(".errorMsg");
 
     const mainUrl = "https://mandarin.api.weniv.co.kr";
     const reqPath = "/user/login";
@@ -55,6 +70,8 @@ function EmailLogin() {
       });
       if (res.data.message === "이메일 또는 비밀번호가 일치하지 않습니다.") {
         error.style.display = "block";
+        onReset();
+        inputRef.current.focus();
       } else {
         history.push("/home");
       }
@@ -76,6 +93,8 @@ function EmailLogin() {
           type="id"
           className="input-email"
           onChange={onEmailHandler}
+          value={email}
+          ref={inputRef}
         />
         <label className="label-pw" htmlFor="password">
           비밀번호
@@ -86,6 +105,7 @@ function EmailLogin() {
           type="password"
           className="input-password"
           onChange={onPasswordHandler}
+          value={password}
         />
         <strong className="errorMsg">
           *이메일 또는 비밀번호가 일치하지 않습니다.
