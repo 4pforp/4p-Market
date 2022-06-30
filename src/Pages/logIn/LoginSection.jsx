d3266595b755cc117caca49fc9f596b3ad16407d
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-06-30 18:15:08 +0900

    🎨:  소셜로그인버튼 props로 구현, 스타일 수정

--- src/Pages/logIn/LoginSection.jsx
+++ src/Pages/logIn/LoginSection.jsx
@@ -2,11 +2,37 @@ import SocialLoginBtn from "./SocialLoginBtn";
 import EmailLoginBtn from "./EmailLoginBtn";
 import "./LoginSection.scss";
 
+const social = [
+  {
+    id: 1,
+    name: "카카오톡",
+    style: "link-kakao",
+  },
+  {
+    id: 2,
+    name: "구글",
+    style: "link-google",
+  },
+  {
+    id: 3,
+    name: "페이스북",
+    style: "link-facebook",
+  },
+];
+
 function LoginSection() {
   return (
     <section className="section-login">
       <h2 className="blind">로그인 섹션입니다.</h2>
-      <SocialLoginBtn />
+      <ul className="list-socialbtn">
+        {social.map((social) => (
+          <SocialLoginBtn
+            key={social.id}
+            name={social.name}
+            style={social.style}
+          />
+        ))}
+      </ul>
       <EmailLoginBtn />
     </section>
   );
