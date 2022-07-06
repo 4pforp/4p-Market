cb172865d21ede423ca49191d886d17bb18190ce
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-07-07 00:41:48 +0900

    🎨: Splash 애니메이션 효과 추가

--- src/Components/splash/Splash.jsx
+++ src/Components/splash/Splash.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 //인트로 splash페이지에서 일정시간 이후 login페이지로 이동하는 기능
 function Splash() {
-  const navigate = useNavigate("/");
+  const navigate = useNavigate();
   const timeout = () => {
     setTimeout(() => {
       navigate("/home");
