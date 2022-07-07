95ee7f5b82fd27431c4c92b638b329b94a199e62
Sam <wtawonlife@gmail.com>
2022-07-07 14:21:27 +0900

    🎨 메인헤더 작업 시작 및 검색 아이콘 이미지 추가

--- src/Components/header/MainHeader.jsx
+++ src/Components/header/MainHeader.jsx
@@ -1,5 +1,15 @@
+import './MainHeader.scss';
+import iconSearch from '../../assets/icon-search.svg';
 function MainHeader() {
-  return;
+  return(
+    <aside className="header-main">
+    <h1 className="header-title">감귤마켓 피드</h1>
+    <button type="button" className="btn-search">
+      <span className="a11y-hidden">검색버튼</span>
+    </button>
+    </aside>
+  )
 }
 
 export default MainHeader;
+
