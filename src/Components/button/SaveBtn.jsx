eb2377ffaa9f06046cfadadb0ddce32ecb54d1f8
waterbinnn <ewaterbin@naver.com>
2022-07-11 04:18:42 +0900

    🎨 버튼 컴포넌트 수정

--- src/Components/button/SaveBtn.jsx
+++ src/Components/button/SaveBtn.jsx
@@ -1,11 +1,11 @@
-import './SaveBtn.scss';
+import "./SaveBtn.scss";
 
-function SaveBtn(props){
-  return(
+function SaveBtn(props) {
+  return (
     <button type="submit" className="btn-save false" disabled>
       {props.text}
     </button>
-  )
+  );
 }
 
 export default SaveBtn;
