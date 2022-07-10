719d58c54cb053d506389bcb643d13f870e2d1bb
Sam <wtawonlife@gmail.com>
2022-07-11 05:13:00 +0900

    🎨 좋아요 버튼 asset파일 추가 및 sample기능 추가

--- src/Components/button/LikeBtn.jsx
+++ src/Components/button/LikeBtn.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import './LikeBtn.scss';
 
 function LikeBtn(){
+ 
+  const[liked, setLiked] = useState({
+    heart : false,
+    number : false
+  });
+
+  function handleLikeClick(){
+    setLiked({heart : !liked.heart , number : !liked.number})
+  }
+
   return(
     <div className="btn-like-box">
-    <button type="button" className="btn-like">
+    <button type="button" className={liked.heart ? "btn-liked" : "btn-like"} onClick={handleLikeClick}>
        <span className="a11y-hidden">좋아요</span>
     </button>
-    <span className="like-number">1</span>
+    <span className="like-number">{liked.number ? "1" : "0"}</span>
     </div>
   )
 }
