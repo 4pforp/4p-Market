4a3a3661c2c68a77ed3a7a8c4fa2c4a6c36562b4
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-07-10 18:09:21 +0900

    🎨: 포스트 Upload페이지 UI 구현

---
+++ src/Pages/post/Upload.jsx
@@ -0,0 +1,43 @@
+import { useRef, useCallback } from "react";
+import UploadNav from "../../Components/navBar/UploadNav";
+import UploadIconBtn from "../../Components/button/UploadIconBtn";
+import profileimg from "../../assets/icon-profile-small.svg";
+import "./Upload.scss";
+
+function Upload() {
+  const textRef = useRef();
+  const handleResizeHeight = useCallback(() => {
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  });
+
+  return (
+    <>
+      <div className="content-wrap-upload">
+        <UploadNav />
+        <form method="post" className="form-upload">
+          <img
+            src={profileimg}
+            alt="프로필 사진입니다."
+            className="proifleimage-upload"
+          />
+          <textarea
+            name="textarea-upload"
+            // onChange={onChange}
+            ref={textRef}
+            className="textarea-upload"
+            placeholder="게시글 입력하기"
+            onInput={handleResizeHeight}
+            maxLength="2000"
+          />
+          <UploadIconBtn
+            className="UploadIcon-uploadpage"
+            img="upload-file.svg"
+            name="upload-post"
+          />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Upload;
