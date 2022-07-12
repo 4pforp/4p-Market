49adfde4c5fafd7cb9d7e500de54f535fe0d0fcc
waterbinnn <ewaterbin@naver.com>
2022-07-13 01:33:08 +0900

    🎨 프로필이미지 컴포넌트 반영

--- src/Pages/chat/chatPage/ChatItem.jsx
+++ src/Pages/chat/chatPage/ChatItem.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import UserInfoBox from "../../../Components/user/UserInfoBox";
 import "./ChatItem.scss";
 
 function ChatItem() {
@@ -9,13 +10,11 @@ function ChatItem() {
     <>
       <li className="wrapper-chatitem">
         <Link to={`/chatroom/${id}`} className="item-chatpage">
-          <div className="img-author chatpage"></div>
-          <div className="wrapper-text-chat">
-            <strong className="text-username">수삐뽀삐</strong>
-            <p className="text-chat">
-              안녕하세요 고양이 캣타워 아직 파나요? 혹시 에눌 되나요?
-            </p>
-          </div>
+          <UserInfoBox
+            type="chatpage"
+            name="수삐뽀삐"
+            subtext="안녕하세요 고양이 캣타워 아직 파나요? 혹시 에눌 되나요?"
+          />
           <strong className="text-chat-date">2022.07.27</strong>
         </Link>
       </li>
--- src/Pages/chat/chatRoom/GetMessage.jsx
+++ src/Pages/chat/chatRoom/GetMessage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Message.scss";
+import UserInfoBox from "../../../Components/user/UserInfoBox";
 
 function GetMessage({ chat, time }) {
   return (
@@ -8,7 +9,7 @@ function GetMessage({ chat, time }) {
       <section className="wrapper-message">
         <h2 className="a11y-hidden">메세지</h2>
         <Link to="/profile">
-          <div className="img-author message"></div>
+          <UserInfoBox type="message" />
         </Link>
         <div className="wrapper-text-message">
           <p className="text-message">{chat}</p>
