76c2f3a6dc0e3f13de44041481e834ee468077d7
Sam <wtawonlife@gmail.com>
2022-07-12 19:52:34 +0900

    🎨user info box 컴포넌트 추가 및 오타 수정

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -2,7 +2,7 @@ import CommentBtn from "../button/CommentBtn";
 import LikeBtn from "../button/LikeBtn";
 import UserMoreBtn from "../button/UserMoreBtn";
 import examplePicture from "../../assets/example-picture.jpg";
-
+import UserInfoBox from "../../Components/user/UserInfoBox";
 import "./Post.scss";
 
 function Post() {
@@ -10,11 +10,9 @@ function Post() {
     <>
       <article className="article-post">
         <h3 className="a11y-hidden">user의 post</h3>
-        <header className="wrapper-author">
-          <div className="img-author post"></div>
-          <strong className="text-username post">애월읍 위니브 감귤농장</strong>
-          <strong className="text-accountname post">@weniv_Mandarin</strong>
-        </header>
+        <UserInfoBox type="post" name="애월읍 위니브 감귤농장" id="@weniv_Mandarin">
+        <UserMoreBtn />
+        </UserInfoBox>
         <main className="contents-post">
           <p className="text-post">
             옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다.
@@ -30,7 +28,6 @@ function Post() {
           </div>
           <strong className="text-post-date">2020년 10월 21일</strong>
         </main>
-        <UserMoreBtn />
       </article>
     </>
   );
