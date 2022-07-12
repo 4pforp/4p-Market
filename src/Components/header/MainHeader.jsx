e859bce9cb455bf9aedc31feff129348206b0f93
Sam <wtawonlife@gmail.com>
2022-07-12 15:10:41 +0900

    🧹scss 속성 순서 컨벤션에 맞게 변경

--- src/Components/header/MainHeader.jsx
+++ src/Components/header/MainHeader.jsx
@@ -5,7 +5,7 @@ function MainHeader() {
   return (
     <header className="container-header">
       <section className="top-bar">
-        <h1 className="header-title">감귤마켓 피드</h1>
+        <h1 className="title-header">감귤마켓 피드</h1>
         <SearchBtn />
       </section>
     </header>
