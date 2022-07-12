d7be09cbf20e9cea0c07131903dd5849fcd5acbf
waterbinnn <ewaterbin@naver.com>
2022-07-12 15:41:36 +0900

    ♻️ css 컨벤션 적용

--- src/Pages/chat/chatPage/ChatItem.jsx
+++ src/Pages/chat/chatPage/ChatItem.jsx
@@ -1,27 +1,22 @@
 import { Link } from "react-router-dom";
 import "./ChatItem.scss";
 
-function ChatItem({ message }) {
+function ChatItem() {
   //임시로 id 지정
   const id = 1;
 
   return (
     <>
-      <li className="chat-item">
-        <Link to={`/chatroom/${id}`} className="chat-item-link">
-          <div className="user-profile-img">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYUM58e7DK8DJVNvNc_iyeVSscQralZds2mT_PZBCInqzfgNM1eIbM1oA24EKZ2ijWDjU&usqp=CAU"
-              alt="username의 프로필사진"
-            />
-          </div>
-          <div className="chat-info">
-            <strong className="user-name">수삐뽀삐</strong>
-            <p className="chat-content">
+      <li className="wrapper-chatitem">
+        <Link to={`/chatroom/${id}`} className="item-chatpage">
+          <div className="img-author chatpage"></div>
+          <div className="wrapper-text-chat">
+            <strong className="text-username">수삐뽀삐</strong>
+            <p className="text-chat">
               안녕하세요 고양이 캣타워 아직 파나요? 혹시 에눌 되나요?
             </p>
           </div>
-          <strong className="chat-date">2022.07.27</strong>
+          <strong className="text-chat-date">2022.07.27</strong>
         </Link>
       </li>
     </>
--- src/Pages/chat/chatRoom/ChatRoom.jsx
+++ src/Pages/chat/chatRoom/ChatRoom.jsx
@@ -3,15 +3,15 @@ import ChatroomFooter from "../../../Components/footer/ChatroomFooter";
 import Chatting from "./Chatting";
 import "./ChatRoom.scss";
 
-/* 유저네임 데이터 가져와야 함 */
-
 function ChatRoom() {
   return (
     <>
       <ChatRoomHeader userName="수삐뽀삐" />
-      <main className="chatting-room-main">
-        <h2 className="a11y-hidden">채팅창</h2>
-        <Chatting />
+      <main className="container-chatroom">
+        <div className="wrapper-chatroom">
+          <h2 className="a11y-hidden">채팅창</h2>
+          <Chatting />
+        </div>
       </main>
       <ChatroomFooter />
     </>
