6a41632aa871c5a0b7b2a810f3fcd5d5be73ce16
Sam <wtawonlife@gmail.com>
2022-07-13 22:06:55 +0900

    🎨 유저 정보 박스 컴포넌트 적용 및 스타일 수정

--- src/Pages/profile/commentPage/CommentList.jsx
+++ src/Pages/profile/commentPage/CommentList.jsx
@@ -1,31 +1,36 @@
 import './CommentList.scss';
 import UserMoreBtn from '../../../Components/button/UserMoreBtn';
-import defaultProfile from '../../../assets/L-profile.svg';
+import UserInfoBox from '../../../Components/user/UserInfoBox';
 
 function CommentList(){
   return(
     <div className="container-comments">
       <ul className="list-comments">
-        <li className="comment">
-        <div className="wrapper-comment-author">
-        <img src={defaultProfile} alt="" className="img-comment-author"/>
-          <span className="text-comment-author-name">나는유저소현</span>
-          <span className="text-comment-time">· 5분전</span>
+        <li className="item-comment">
+        <UserInfoBox type="comment" name="나는유저소현">
+          <span className="text-comment-time">· 4분전</span>
           <UserMoreBtn/>
-        </div>
+        </UserInfoBox>
         <p className="content-comment">
-          댓글남기고 갑니다. 덩기덕 쿵더러러러러러러러러 덩기덕 쿵덕~!
+          댓글남기고 갑니다. 덩기덕
         </p>
         </li>
-        <li className="comment">
-        <div className="wrapper-comment-author">
-        <img src={defaultProfile} alt="" className="img-comment-author"/>
-          <span className="text-comment-author-name">Jane Doe</span>
-          <span className="text-comment-time">· 10분전</span>
+        <li className="item-comment">
+        <UserInfoBox type="comment" name="아무개">
+          <span className="text-comment-time">· 11분전</span>
           <UserMoreBtn/>
-        </div>
+        </UserInfoBox>
         <p className="content-comment">
           knock knock knock knock knock knock knock knock anybody there? 
+        </p>
+        </li>
+        <li className="item-comment">
+        <UserInfoBox type="comment" name="엄궁동">
+          <span className="text-comment-time">· 12분전</span>
+          <UserMoreBtn/>
+        </UserInfoBox>
+        <p className="content-comment">
+          비치비치 다비치~
         </p>
         </li>
       </ul>
