3cd4e47fb65543d6e2449a4ed1fb9b52ed322e8e
Sam <wtawonlife@gmail.com>
2022-07-13 22:19:40 +0900

    🎨 UserInfoBox 컴포넌트 페이지에 구현 완료 및 css 수정

---
+++ src/Pages/post/Post.jsx
@@ -0,0 +1,14 @@
+import UploadNav from "../../components/navBar/UploadNav";
+import CommentFooter from "../../components/footer/CommentFooter";
+
+function Post() {
+  return (
+    <>
+      <UploadNav />
+      <h1>게시물작성페이지</h1>
+      <CommentFooter />
+    </>
+  );
+}
+
+export default Post;
---
+++ src/Pages/profile/Profile.jsx
@@ -0,0 +1,14 @@
+import BasicNav from "../../components/navBar/BasicNav";
+import MainFooter from "../../components/footer/MainFooter";
+
+function Profile() {
+  return (
+    <>
+      <BasicNav />
+      <h1>프로필페이지 </h1>
+      <MainFooter />
+    </>
+  );
+}
+
+export default Profile;
