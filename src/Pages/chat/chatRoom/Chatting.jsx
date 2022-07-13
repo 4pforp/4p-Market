ebb272fc55eadf727eedd166a36d043d5826a263
waterbinnn <ewaterbin@naver.com>
2022-07-13 14:51:10 +0900

    ♻️ 채팅 css 수정

--- src/Pages/chat/chatRoom/Chatting.jsx
+++ src/Pages/chat/chatRoom/Chatting.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./ChatRoom.scss";
 import GetMessage from "./GetMessage";
 import SendMessage from "./SendMessage";
-import LogoImg from "../../../assets/L-profile.svg";
-
-// 프로필이미지, 채팅내용, 시간 유저데이터 가져와야 함
 
 function Chatting() {
   return (
@@ -12,7 +9,6 @@ function Chatting() {
       <section>
         <h2 className="a11y-hidden">채팅창</h2>
         <GetMessage
-          userProfileImg={LogoImg}
           chat="안녕하세요 고양이 캣타워 아직 파나요? 혹시 에눌 되나요?"
           time="17:23"
         />
