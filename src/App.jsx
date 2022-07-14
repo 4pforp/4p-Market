fa33f0361f3710c25d238a67211e8a1c79486706
usablelpaper <usablepaper@gmail.com>
2022-07-14 23:54:34 +0900

    🧹 로그아웃 상태에서 url접근시 Not Found 페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,7 @@ function App() {
                 <Route path="/home" element={<Home />}></Route>
                 <Route path="/login" element={<EmailLogin />}></Route>
                 <Route path="/signup" element={<SignUp />}></Route>
+                <Route path={"*"} element={<NotFound />}></Route>
               </>
             )}
           </Routes>
