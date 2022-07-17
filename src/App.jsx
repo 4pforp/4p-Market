960bc9291bb995672309d7e3defebef71cfde08e
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-07-17 19:32:36 +0900

    ♻️: useContext 분리 및 리펙토링

--- src/App.jsx
+++ src/App.jsx
@@ -17,79 +17,64 @@ import ProfileEdit from "./pages/profileEdit/ProfileEdit";
 import UserSearch from "./pages/home/userSearch/UserSearch";
 import CommentPage from "./pages/profile/commentPage/CommentPage";
 import NotFound from "./pages/notFound/NotFound";
-import UserContext from "./context/UserContext";
-
-function App() {
-  const [token, setToken] = useState(localStorage.getItem("token"));
-  const [myAccountname, setMyAccountname] = useState(
-    localStorage.getItem("accountname")
-  );
-  const [myUsername, setMyUsername] = useState(
-    localStorage.getItem("username")
-  );
-  const [myImage, setMyImage] = useState(localStorage.getItem("image"));
-  const [myIntro, setMyIntro] = useState(localStorage.getItem("intro"));
-  const [myEmail, setMyEmail] = useState(localStorage.getItem("email"));
+import UserContext, { UserContextProvider } from "./context/UserContext";
+import SplashContext, { SplashContextProvider } from "./context/SplashContext";
+import Splash from "./components/splash/Splash";
+import { useContext } from "react";
+const Main = () => {
+  const { token, myAccountname } = useContext(UserContext);
+  const { splash } = useContext(SplashContext);
   return (
     <div className="App">
-      <UserContext.Provider
-        value={{
-          token,
-          setToken,
-          myAccountname,
-          setMyAccountname,
-          myUsername,
-          setMyUsername,
-          myIntro,
-          setMyIntro,
-          myEmail,
-          setMyEmail,
-          myImage,
-          setMyImage,
-        }}
-      >
-        <BrowserRouter>
-          <Routes>
-            {token ? (
-              <>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/login" element={<EmailLogin />}></Route>
-                <Route path="/signup" element={<SignUp />}></Route>
-                <Route path="/search" element={<UserSearch />}></Route>
-                <Route path="/chat" element={<ChatPage />}></Route>
-                <Route path="/chatroom/:id" element={<ChatRoom />}></Route>
-                <Route path="/upload" element={<UploadPost />}></Route>
-                <Route
-                  path={"/" + myAccountname}
-                  element={<MyProfile />}
-                ></Route>
-                <Route path="/:accountname" element={<UserProfile />}></Route>
-                <Route
-                  path="/:accountname/followers"
-                  element={<Followers />}
-                ></Route>
-                <Route
-                  path="/:accountname/followings"
-                  element={<Followings />}
-                ></Route>
-                <Route path="/comment" element={<CommentPage />}></Route>
-                <Route path="/product" element={<Product />}></Route>
-                <Route path="/profileedit" element={<ProfileEdit />}></Route>
-                <Route path={"*"} element={<NotFound />}></Route>
-              </>
-            ) : (
-              // TODO 만약 url로 접근하려고 할 때 로그인 창으로 넘어가도록 추가
-              <>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/login" element={<EmailLogin />}></Route>
-                <Route path="/signup" element={<SignUp />}></Route>
-                <Route path={"*"} element={<NotFound />}></Route>
-              </>
-            )}
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
+      {splash ? null : <Splash />}
+      <BrowserRouter>
+        <Routes>
+          {token ? (
+            <>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/login" element={<EmailLogin />}></Route>
+              <Route path="/signup" element={<SignUp />}></Route>
+              <Route path="/search" element={<UserSearch />}></Route>
+              <Route path="/chat" element={<ChatPage />}></Route>
+              <Route path="/chatroom/:id" element={<ChatRoom />}></Route>
+              <Route path="/upload" element={<UploadPost />}></Route>
+              <Route path={"/" + myAccountname} element={<MyProfile />}></Route>
+              <Route path="/:accountname" element={<UserProfile />}></Route>
+              <Route
+                path="/:accountname/followers"
+                element={<Followers />}
+              ></Route>
+              <Route
+                path="/:accountname/followings"
+                element={<Followings />}
+              ></Route>
+              <Route path="/comment" element={<CommentPage />}></Route>
+              <Route path="/product" element={<Product />}></Route>
+              <Route path="/profileedit" element={<ProfileEdit />}></Route>
+              <Route path={"*"} element={<NotFound />}></Route>
+            </>
+          ) : (
+            // TODO 만약 url로 접근하려고 할 때 로그인 창으로 넘어가도록 추가
+            <>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/login" element={<EmailLogin />}></Route>
+              <Route path="/signup" element={<SignUp />}></Route>
+              <Route path={"*"} element={<NotFound />}></Route>
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
     </div>
+  );
+};
+
+function App() {
+  return (
+    <SplashContextProvider>
+      <UserContextProvider>
+        <Main />
+      </UserContextProvider>
+    </SplashContextProvider>
   );
 }
 
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const UserContext = createContext();
-
 export default UserContext;
+const UserContextProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [myAccountname, setMyAccountname] = useState(
+    localStorage.getItem("accountname")
+  );
+  const [myUsername, setMyUsername] = useState(
+    localStorage.getItem("username")
+  );
+  const [myImage, setMyImage] = useState(localStorage.getItem("image"));
+  const [myIntro, setMyIntro] = useState(localStorage.getItem("intro"));
+  const [myEmail, setMyEmail] = useState(localStorage.getItem("email"));
+  return (
+    <UserContext.Provider
+      value={{
+        token,
+        setToken,
+        myAccountname,
+        setMyAccountname,
+        myUsername,
+        setMyUsername,
+        myIntro,
+        setMyIntro,
+        myEmail,
+        setMyEmail,
+        myImage,
+        setMyImage,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContextProvider };
