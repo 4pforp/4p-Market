dbb6418d44084e2b0a770be9121218ece93f5daa
usablelpaper <usablepaper@gmail.com>
2022-07-18 16:26:44 +0900

    🔧 포스트 상세페이지 구현

--- src/components/button/CommentBtn.jsx
+++ src/components/button/CommentBtn.jsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import "./CommentBtn.scss";
 
-function CommentBtn({ commentcount, postid }) {
+function CommentBtn({ commentcount, postid, from, accountname }) {
+  const navigate = useNavigate();
+  const style = from === "comment" ? { pointerEvents: "none" } : null;
+
+  function handleClick() {
+    from === "home" && navigate(accountname + "/" + postid);
+    from === "profile" && navigate(postid);
+  }
   return (
     <div className="wrapper-btn-comment">
-      <button type="button" className="btn-comment">
+      <button
+        type="button"
+        className="btn-comment"
+        style={style}
+        onClick={handleClick}
+      >
         <span className="a11y-hidden">댓글</span>
       </button>
       <span className="text-comment-num">{commentcount || 0}</span>
