700eb22a956a36d4b44b0c31eb1a4a205b0997f6
waterbinnn <ewaterbin@naver.com>
2022-07-19 10:37:17 +0900

    🔧 버튼에 모달 활성화 기능 추가

--- src/components/button/UserMoreBtn.jsx
+++ src/components/button/UserMoreBtn.jsx
@@ -1,11 +1,13 @@
-import './UserMoreBtn.scss';
+import "./UserMoreBtn.scss";
 
-function UserMoreBtn(){
-  return(
-    <button type="button" className="btn-user-more">
-      <span className="a11y-hidden">삭제 혹은 신고하기</span>
-    </button>
-  )
+function UserMoreBtn({ handleClick }) {
+  return (
+    <>
+      <button type="button" className="btn-user-more" onClick={handleClick}>
+        <span className="a11y-hidden">삭제 혹은 신고하기</span>
+      </button>
+    </>
+  );
 }
 
 export default UserMoreBtn;
