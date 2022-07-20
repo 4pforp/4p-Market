66990894e75dee303e12a13116aa787add26b14e
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-07-21 04:59:17 +0900

    🔧: UploadProductPage 이미지 업로드, 버튼 활성화 기능 구현

--- src/pages/uploadProductPage/UploadProductPage.jsx
+++ src/pages/uploadProductPage/UploadProductPage.jsx
@@ -1,38 +1,37 @@
+import { useState } from "react";
 import SaveHeader from "../../components/header/SaveHeader";
+import UploadProductImg from "./uploadProductImg/UploadProductImg";
 import ProductInput from "./uploadProductInput/UploadProductInput";
-import PictureIcon from "../../assets/upload-file-gray.svg";
 import "./UploadProductPage.scss";
 
 function UploadProductPage() {
+  const [itemName, setItemName] = useState("");
+  const [price, setPrice] = useState("");
+  const [link, setLink] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  const [disabled, setIsDisabled] = useState(true);
+  const [itemImage, setItemImage] = useState("");
+
   return (
     <>
-      <SaveHeader />
-      <h1 className="a11y-hidden">상품등록페이지</h1>
       <form
         method="POST"
         encType="multipart/form-data"
         className="container-form-product"
       >
-        <p className="text-upload-img">이미지등록</p>
-        <div className="wrapper-productimg">
-          <label htmlFor="choose-img" className="label-upload-icon">
-            <img src={PictureIcon} alt="upload" />
-          </label>
-          <input
-            type="file"
-            name="img-product"
-            id="choose-img"
-            accept="image/*"
-          ></input>
-        </div>
-        <div className="container-input-product">
-          <ProductInput
-            name="productname"
-            placeholder="2~15자 이내여야 합니다."
-          />
-          <ProductInput name="price" placeholder="숫자만 입력 가능합니다." />
-          <ProductInput name="salelink" placeholder="URL을 입력해 주세요" />
-        </div>
+        <SaveHeader isActive={isActive} disabled={disabled} />
+        <h1 className="a11y-hidden">상품등록페이지</h1>
+        <UploadProductImg itemImage={itemImage} setItemImage={setItemImage} />
+        <ProductInput
+          itemName={itemName}
+          setItemName={setItemName}
+          price={price}
+          setPrice={setPrice}
+          link={link}
+          setLink={setLink}
+          setIsActive={setIsActive}
+          setIsDisabled={setIsDisabled}
+        />
       </form>
     </>
   );
--- src/pages/uploadProductPage/uploadProductInput/UploadProductInput.jsx
+++ src/pages/uploadProductPage/uploadProductInput/UploadProductInput.jsx
@@ -1,11 +1,91 @@
-function ProdcutInput({ name, placeholder }) {
+import { useEffect } from "react";
+
+function ProdcutInput({
+  itemName,
+  price,
+  link,
+  setItemName,
+  setPrice,
+  setLink,
+  setIsActive,
+  setIsDisabled,
+}) {
+  // 저장 버튼활성화 기능
+  useEffect(() => {
+    if (itemName.length > 1 && price.length !== 0 && link.length !== 0) {
+      setIsActive(true);
+      setIsDisabled(false);
+    } else {
+      setIsActive(false);
+      setIsDisabled(true);
+    }
+  });
+
+  // 가격인풋창 3자리마다 콤마 붙여주는 기능
+  const inputPriceFormat = (str) => {
+    const comma = (str) => {
+      str = String(str);
+      return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, "$1,");
+    };
+    const uncomma = (str) => {
+      str = String(str);
+      return str.replace(/[^\d]+/g, "");
+    };
+    return comma(uncomma(str));
+  };
+
+  function handleChangeProductName(e) {
+    setItemName(e.target.value);
+  }
+  function handleChangePrice(e) {
+    setPrice(inputPriceFormat(e.target.value));
+  }
+  function handleChangeSaleLink(e) {
+    setLink(e.target.value);
+  }
   return (
-    <div className={`wrapper-${name}`}>
-      <label htmlFor={`input-${name}`} className={`label-${name}`}>
-        상품명
-      </label>
-      <input id={`input-${name}`} type="text" placeholder={placeholder}></input>
-    </div>
+    <>
+      <div className="container-input-product">
+        <div className="wrapper-productname">
+          <label htmlFor="input-productname" className="label-productname">
+            상품명
+          </label>
+          <input
+            id="input-productname"
+            type="text"
+            placeholder="2~15자 "
+            maxLength="16"
+            minLength="2"
+            onChange={handleChangeProductName}
+            value={itemName}
+          ></input>
+        </div>
+        <div className="wrapper-price">
+          <label htmlFor="input-price" className="label-price">
+            가격
+          </label>
+          <input
+            id="input-price"
+            type="text"
+            placeholder="숫자만 입력 가능합니다."
+            onChange={handleChangePrice}
+            value={price}
+          ></input>
+        </div>
+        <div className="wrapper-salelink">
+          <label htmlFor="input-salelink" className="label-salelink">
+            판매 링크
+          </label>
+          <input
+            id="input-salelink"
+            type="text"
+            placeholder="URL을 입력해주세요"
+            onChange={handleChangeSaleLink}
+            value={link}
+          ></input>
+        </div>
+      </div>
+    </>
   );
 }
 
