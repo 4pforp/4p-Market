96ac74aa5ca07bc9ed4a5565441467b0a16109b6
usablelpaper <usablepaper@gmail.com>
2022-07-25 16:18:35 +0900

    🔧 피드 페이지 무한스크롤 기능 추가

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { React, useContext, useEffect, useState } from "react";
+import { React, useContext, useEffect, useState, useRef } from "react";
 import UserContext from "../../context/UserContext";
 import axios from "axios";
 import PostList from "../../components/post/PostList";
@@ -15,12 +15,20 @@ function HomePage() {
   const { token, initialToken } = useContext(UserContext);
   const [posts, setPosts] = useState();
   const [view, setView] = useState("pending");
+  const Container = useRef();
+  const [reloadNeed, setReloadNeed] = useState(false);
+  const [reloadStop, setReloadStop] = useState(false);
+  const [updatedCount, setUpdatedCount] = useState(0);
+  const [skip, setSkip] = useState(0);
 
   useEffect(() => {
-    async function getPost() {
-      // 팔로잉 유저의 포스트 받아오기
-      const url = "https://mandarin.api.weniv.co.kr/post/feed";
-
+    // 실시간 업로드 댓글 반영 함수
+    async function getPosts() {
+      const url =
+        "https://mandarin.api.weniv.co.kr/post/feed" +
+        "/?limit=15" +
+        "&skip=" +
+        skip;
       try {
         const res = await axios.get(url, {
           headers: {
@@ -28,15 +36,69 @@ function HomePage() {
             "Content-type": "application/json",
           },
         });
+        setSkip(0);
         setPosts(res.data.posts);
         setView("fulfilled");
       } catch (err) {
-        console.error(err);
         setView("rejected");
       }
     }
-    initialToken && getPost();
-  }, [initialToken]);
+    getPosts();
+  }, [token, view]);
+
+  useEffect(() => {
+    // 화면 마지막에 도달하면 ReloadNeed!
+    function infinitScoll() {
+      const targetHeight = Math.floor(
+        Container.current.getBoundingClientRect().height
+      );
+      const currentScrollY = Math.floor(
+        window.scrollY + window.innerHeight - 50
+      );
+      targetHeight < currentScrollY && setReloadNeed(true);
+    }
+
+    window.addEventListener("scroll", infinitScoll);
+
+    // 스크롤시 데이터 추가 요청 함수
+    async function getPosts() {
+      const url =
+        "https://mandarin.api.weniv.co.kr/post/feed" +
+        "/?limit=15" +
+        "&skip=" +
+        skip;
+      try {
+        const res = await axios.get(url, {
+          headers: {
+            Authorization: token,
+            "Content-type": "application/json",
+          },
+        });
+        // 첫 데이터면 전체 데이터 받아오기/데이터가 있으면 스프레드 문법 사용하여 추가하기
+        if (skip === 0) {
+          setPosts(res.data.posts);
+        } else {
+          setPosts([...posts, ...res.data.posts]);
+        }
+        // 배열이 비어있으면 데이터 요청 차단
+        res.data.posts.length === 0 && setReloadStop(true);
+        setUpdatedCount(updatedCount + 1);
+        setReloadNeed(false);
+        setSkip(skip + 15);
+      } catch (err) {
+        setView("rejected");
+      }
+    }
+    // 화면 마지막에 도달하면 infinite scroll 시작
+    if (reloadNeed === true) {
+      reloadStop || getPosts();
+    }
+    // 언마운트시에 스크롤이벤트 발생하지 않도록!
+    return () => {
+      window.removeEventListener("scroll", infinitScoll);
+    };
+  }, [token, posts, updatedCount, reloadNeed, skip, initialToken, view]);
+
   return (
     <>
       {sessionStorage.getItem("splash") ? null : <Splash />}
@@ -49,7 +111,7 @@ function HomePage() {
               <main className="main-home feed">
                 <section className="container-post feed">
                   <div className="wrapper-post">
-                    <ol className="list-posts">
+                    <ol className="list-posts" ref={Container}>
                       <PostList post={posts} from="home" />
                     </ol>
                   </div>
