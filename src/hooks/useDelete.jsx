66cb853d39d47f54705ee98147a7b50f5d1acf49
waterbinnn <ewaterbin@naver.com>
2022-07-31 02:25:50 +0900

    🔧 댓글 삭제 시 댓글 수 카운트 다운 구현

--- src/hooks/useDelete.jsx
+++ src/hooks/useDelete.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import UserContext from "../context/UserContext";
 import axios from "axios";
 
@@ -6,17 +6,28 @@ import axios from "axios";
 function useDelete() {
   const { token } = useContext(UserContext);
   const [isUpdate, setIsUpdate] = useState(false);
+
+  //댓글 삭제시 카운트 다운
+  function handleCount() {
+    const commentCount = document.querySelector(".text-comment-num");
+    commentCount.innerHTML--;
+  }
+
   async function remove(backUrl) {
     try {
-      const res = await axios.delete("https://mandarin.api.weniv.co.kr/" + backUrl, {
-        headers: {
-          Authorization: token,
-          "Content-type": "application/json",
-        },
-      });
+      const res = await axios.delete(
+        "https://mandarin.api.weniv.co.kr/" + backUrl,
+        {
+          headers: {
+            Authorization: token,
+            "Content-type": "application/json",
+          },
+        }
+      );
       // 삭제 처리 되었을 때 isUpdate의 상태값을 true로 변경
       if (parseInt(res.data.status) === 200) {
         setIsUpdate(!isUpdate);
+        handleCount();
       }
     } catch (err) {
       console.error(err);
